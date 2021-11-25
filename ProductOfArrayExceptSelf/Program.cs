c52c576bf3e96727c930c88edfeeb2d4d237b35d
Sean <seanduddy@hotmail.co.uk>
2021-11-25 14:35:11 +0000

    Update Program.cs
    Updated so that the need for dictionaries was removed. There is no need.

--- ProductOfArrayExceptSelf/Program.cs
+++ ProductOfArrayExceptSelf/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 //238.Product of Array Except Self
-//Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+//Given an integer array nums, return an array answer such that answer[i]
+//is equal to the product of all the elements of nums except nums[i].
 
 //Example 1:
 //Input: nums = [1, 2, 3, 4]
@@ -24,34 +23,28 @@ namespace ProductOfArrayExceptSelf
             watch.Start();
 
             //Array for testing.
-            int[] nums = { 1, 2, 3, 4 };
+            int[] nums = { -1, 1, 0, -3, 3 };
 
-            //Two dictionaries, tbh could have down this with arrays but sall good man.
-            var dictionary = new Dictionary<int, int>();
-            var dictionaryProduct = new Dictionary<int, int>();
+            int lPointer = new int();
+            int rPointer = new int();
 
-            //Load array into dictionary.
-            for (int x = 0; x < nums.Length; x++)
-            {
-                dictionary.Add(x, nums[x]);
-            }
+            //New output array, created to be the same length as input array.
+            int[] product = new int[nums.Length];
 
             //Loop through each key item in the dictionary.
-            foreach (KeyValuePair<int, int> element in dictionary)
+            for (lPointer = 0; lPointer < nums.Length; lPointer++)
             {
-                //Initialising variable, doesnt work if zero.
-                var product = 1;
+                //Initialising first element in pointer product, doesnt work if zero.
+                product[lPointer] = 1;
 
                 //Loop through each element within the wrray and start multiplying them together.
-                for (int x = 0; x < nums.Length; x++)
+                for (rPointer = 0; rPointer < nums.Length; rPointer++)
                 {
                     //Skip if self.
-                    if (x != element.Key)
+                    if (lPointer != rPointer)
                     {
                         //Sum the products.
-                        product = product * nums[x];
-                        //Store in dictionary.
-                        dictionaryProduct[element.Key] = product;
+                        product[lPointer] = product[lPointer] * nums[rPointer];
                     }
                 }
             }
@@ -59,9 +52,9 @@ namespace ProductOfArrayExceptSelf
             //Parse out.
             var outputString = "{";
 
-            foreach (KeyValuePair<int, int> element in dictionaryProduct)
+            for (int x = 0; x < product.Length; x++)
             {
-                outputString += String.Concat(element.Value, ",");
+                outputString += String.Concat(product[x], ",");
             }
 
             outputString = outputString.Substring(0,outputString.Length - 1);
